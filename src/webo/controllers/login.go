7c63fe2c88a1f5e531e88f742d48e3be5b643628
zixinjian <zixinjian@gmail.com>
2015-07-15 06:59:50 +0800

    no message

--- src/webo/controllers/login.go
+++ src/webo/controllers/login.go
@@ -1,52 +1,52 @@
 package controllers
+
 import (
-    "github.com/astaxie/beego"
-    "webo/models/userMgr"
-    "webo/models/rpc"
-    "fmt"
+	"fmt"
+	"github.com/astaxie/beego"
+	"webo/models/rpc"
+	"webo/models/userMgr"
 )
 
 type LoginController struct {
-    beego.Controller
+	beego.Controller
 }
 
 func (this *LoginController) Get() {
-//    role := this.GetSession("role")
-//    if role == nil {
-    this.Redirect("/static/frame/login.html", 302)
-//    } else {
-//        this.Redirect("/static/main.html", 302)
-//    }
+	//    role := this.GetSession("role")
+	//    if role == nil {
+	this.Redirect("/static/frame/login.html", 302)
+	//    } else {
+	//        this.Redirect("/static/main.html", 302)
+	//    }
 }
 func (this *LoginController) Post() {
-    username := this.GetString("login_username")
-    password := this.GetString("login_password")
+	username := this.GetString("login_username")
+	password := this.GetString("login_password")
 
-    loginRet := rpc.JsonResult{}
-    if username == "" || password == "" {
-        loginRet.Result="请输入用户名和密码！"
-    }
+	loginRet := rpc.JsonResult{}
+	if username == "" || password == "" {
+		loginRet.Result = "请输入用户名和密码！"
+	}
 
-    user, err := userMgr.GetUser(username, password)
-    if err != nil {
-        fmt.Println("err", err)
-        loginRet.Result="用户名或密码错误！"
-    } else {
-        this.SetSession("username", user.Username)
-        this.SetSession("role", user.Role)
-        loginRet.Ret="success"
-    }
-    this.Data["json"] = &loginRet
-    this.ServeJson()
+	user, err := userMgr.GetUser(username, password)
+	if err != nil {
+		fmt.Println("err", err)
+		loginRet.Result = "用户名或密码错误！"
+	} else {
+		this.SetSession("username", user.Username)
+		this.SetSession("role", user.Role)
+		loginRet.Ret = "success"
+	}
+	this.Data["json"] = &loginRet
+	this.ServeJson()
 }
 
 type LogoutController struct {
-    beego.Controller
+	beego.Controller
 }
 
 func (this *LogoutController) Get() {
-//    fmt.Println("logout")
-    this.DelSession("role")
-    this.Redirect("/", 302)
+	//    fmt.Println("logout")
+	this.DelSession("role")
+	this.Redirect("/", 302)
 }
-
--- src/webo/controllers/ui.go
+++ src/webo/controllers/ui.go
@@ -1,23 +1,24 @@
 package controllers
 
 import (
-    "github.com/astaxie/beego"
-    "fmt"
+	"fmt"
+	"github.com/astaxie/beego"
 )
 
 type UiController struct {
-    beego.Controller
+	beego.Controller
 }
-func (this *UiController) Add(){
-    fmt.Println("requestBosy", this.Ctx.Input.RequestBody)
-    fmt.Println("params", this.Ctx.Input.Params)
-    fmt.Println("requestBosy", this.Input()["id"])
-    fmt.Println("ge", this.GetString("xx"))
-    hi, ok := this.Ctx.Input.Params[":hi"]
-    if !ok {
-        fmt.Println("hi", hi)
-    }
-    this.Data["Service"] = "userService"
-    this.Data["Method"] = "add"
-    this.TplNames = "add.html"
+
+func (this *UiController) Add() {
+	fmt.Println("requestBosy", this.Ctx.Input.RequestBody)
+	fmt.Println("params", this.Ctx.Input.Params)
+	fmt.Println("requestBosy", this.Input()["id"])
+	fmt.Println("ge", this.GetString("xx"))
+	hi, ok := this.Ctx.Input.Params[":hi"]
+	if !ok {
+		fmt.Println("hi", hi)
+	}
+	this.Data["Service"] = "userService"
+	this.Data["Method"] = "add"
+	this.TplNames = "add.html"
 }
